8d1a2c65ee670746a7ad68cd1d2f1a7bf0b4ee8e
Ivan Prybolovetz <iprybolovetz@gmail.com>
2020-04-05 18:25:42 +0300

    Add Button to go to another ViewController

---
+++ PassDataProject/SecondViewController.swift
@@ -0,0 +1,29 @@
+//
+//  SecondViewController.swift
+//  PassDataProject
+//
+//  Created by Ivan Prybolovetz on 4/5/20.
+//  Copyright © 2020 Ivan Prybolovetz. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+    
+     var login: String!
+    @IBOutlet weak var label: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        guard let login = login else { return }
+        label.text = "Hello, \(login)"
+        
+    }
+    
+    
+    @IBAction func goBackTapped(_ sender: UIButton) {
+        performSegue(withIdentifier: "uwindSegue", sender: nil)
+    }
+    
+}
--- PassDataProject/ViewController.swift
+++ PassDataProject/ViewController.swift
@@ -9,12 +9,25 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var loginTF: UITextField!
+    @IBOutlet weak var passwordTF: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
-
+    
+    @IBAction func loginTapped(_ sender: UIButton) {
+        performSegue(withIdentifier: "detailSegue", sender: nil)
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        guard let dvc = segue.destination as? SecondViewController else { return }
+        dvc.login = loginTF.text
+    }
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.view.endEditing(true)
+    }
+    
 }
 
